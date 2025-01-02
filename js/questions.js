979dbfb0d2aebce5cd69da2d1259d3d71dbe1a8d
hani seid <146775210+hani2628@users.noreply.github.com>
2025-01-02 00:44:08 -0800

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "what is the capital city of japan?",
+    answer: "tokyo",
+    options: [
+      "addis ababa",
+      "tokyo",
+      "New York",
+      "paris"
+    ]
+  },
+    {
+    numb: 2,
+    question: "who is the president of russia?",
+    answer: "putin",
+    options: [
+      "donald trump",
+      "joe biden",
+      "putin",
+      "sergy lavrov"
+    ]
+  },
+    {
+    numb: 3,
+    question: "what is the biggest planet in our solar system?",
+    answer: "jupiter",
+    options: [
+      "earth",
+      "mars",
+      "uranus",
+      "jupiter"
+    ]
+  },
+    {
+    numb: 4,
+    question: "what coninent is USA in?",
+    answer: "north america",
+    options: [
+      "africa",
+      "asia",
+      "north america",
+      "oceania"
+    ]
+  },
+    {
+    numb: 5,
+    question: "what continent has the most countries?",
+    answer: "africa",
+    options: [
+      "USA",
+      "north america",
+      "africa",
+      "asia"
+    ]
+  },
+  {
+    numb: 6,
+    question: "what is the biggest ciuntry in the world?",
+    answer: "russia",
+    options: [
+      "russia",
+      "china",
+      "USA",
+      "africa"
+    ]
+  },
+  {
+    numb: 7,
+    question: "how many continents are there?",
+    answer: "7",
+    options: [
+      "100",
+      "7",
+      "5",
+      "0"
+    ]
+  },
+  {
+    numb: 8,
+    question: "who won the 2022 fifa world cup?",
+    answer: "france",
+    options: [
+      "brazil",
+      "germany",
+      "italy",
+      "france"
+    ]
+  },
+  {
+    numb: 9,
+    question: "what country has the highest population in the world?",
+    answer: "india",
+    options: [
+      "china",
+      "brazil",
+      "india",
+      "USA"
+    ]
+  },
+  {
+    numb: 10,
+    question: "what is the fastest animal?",
+    answer: "cheetah",
+    options: [
+      "elephant",
+      "cheetah",
+      "lion",
+      "cat"
+    ]
+  },
+
+];
